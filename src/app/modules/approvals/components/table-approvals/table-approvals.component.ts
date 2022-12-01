8d59c7ee8f31d3bde03a8991e14411332dd7cb8b
Levi <levi962009@hotmail.com>
2022-12-01 12:15:05 -0600

    changes

--- src/app/modules/approvals/components/table-approvals/table-approvals.component.ts
+++ src/app/modules/approvals/components/table-approvals/table-approvals.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { TravelRequestService } from '../../../../services/travel-request.service';
+import { TravelRequest } from '../../../../models/TravelRequest.model';
 
 @Component({
   selector: 'app-table-approvals',
@@ -8,12 +10,24 @@ import { Router } from '@angular/router';
 })
 export class TableApprovalsComponent implements OnInit {
 
+  public travel_draft: TravelRequest[] = []
+
   constructor(
-    private _router:Router
+    private _router:Router,
+    private _travelService: TravelRequestService
   ) { }
 
   ngOnInit(): void {
+    this.getTravelRequest()
   }
+
+  getTravelRequest(){
+    this._travelService.getTravelRequest().subscribe((data: any) => {
+      this.travel_draft = data.filter((travels:TravelRequest) => travels.status === 'draft')
+      console.log(data);
+    })
+  }
+
   goToMenuApprovals(){
     this._router.navigateByUrl('/approvals/approvals-travel/add-travel-request')
   }
