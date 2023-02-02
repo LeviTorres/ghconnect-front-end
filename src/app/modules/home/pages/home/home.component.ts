8c717a307067a4ee36cda2b9168767459255ce5c
Ezequiel Lino <sonisombra@gmail.com>
2023-02-02 16:23:02 -0600

    changes

--- src/app/modules/home/pages/home/home.component.ts
+++ src/app/modules/home/pages/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormArray, FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -8,62 +9,64 @@ import { FormArray, FormControl } from '@angular/forms';
 })
 export class HomeComponent implements OnInit {
   public servicesTemp: any[] = []
-  public servicesFilters:any[] = []
+  public servicesFilters: any[] = []
   public services: any[] = [
-    { name: 'Aprobaciones', icon: '../../../../../assets/newIcons/Aprobaciones.svg', type: 'Recursos Humanos' },
-    { name: 'Arrendamiento', icon: '../../../../../assets/newIcons/Arrendamiento.svg', type: 'Ventas' },
-    { name: 'Asistencia', icon: '.../../../../../assets/newIcons/Asistencia.svg', type: 'Recursos Humanos' },
-    { name: 'Clientes', icon: '../../../../../assets/newIcons/Clientes.svg', type: 'Contabilidad' },
-    { name: 'Comidas', icon: '../../../../../assets/newIcons/Comidas.svg', type: 'Recursos Humanos' },
-    { name: 'Compras', icon: './../../../../assets/newIcons/Compras.svg', type: 'Contabilidad' },
-    { name: 'Contratos y Subcontratos', icon: '../../../../../assets/newIcons/Contratos y subcontratos.svg', type: 'Operaciones' },
-    { name: 'CRM', icon: '../../../../../assets/newIcons/CRM.svg', type: 'Ventas' },
-    { name: 'Cuadro Comparativo', icon: '../../../../../assets/newIcons/Cuadro comparativo.svg', type: 'Operaciones' },
-    { name: 'e-Learning', icon: '../../../../../assets/newIcons/eLearning.svg', type: 'Productividad' },
-    { name: 'Empleados', icon: '../../../../../assets/newIcons/Empleados.svg', type: 'Recursos Humanos' },
-    { name: 'Encuestas', icon: '../../../../../assets/newIcons/Encuestas.svg', type: 'Productividad' },
-    { name: 'Eventos', icon: '../../../../../assets/newIcons/Eventos.svg', type: 'Productividad' },
-    { name: 'Facturacion', icon: '../../../../../assets/newIcons/Facturación.svg', type: 'Contabilidad' },
-    { name: 'Flota', icon: '../../../../../assets/newIcons/Flota.svg', type: 'Operaciones' },
-    { name: 'Gestion de Activos', icon: '../../../../../assets/newIcons/Gestión de Archivos.svg', type: 'Contabilidad' },
-    { name: 'Inventario', icon: '../../../../../assets/newIcons/Inventario.svg', type: 'Contabilidad' },
-    { name: 'Mantenimiento', icon: '../../../../../assets/newIcons/Mantenimiento.svg', type: 'Operaciones' },
-    { name: 'Nomina', icon: '../../../../../assets/newIcons/Nómina.svg', type: 'Recursos Humanos' },
-    { name: 'Organigrama', icon: '../../../../../assets/newIcons/Organigrama.svg', type: 'Recursos Humanos' },
-    { name: 'Pedidos', icon: '../../../../../assets/newIcons/Pedidos.svg', type: 'Ventas' },
-    { name: 'Precio Unitario', icon: '../../../../../assets/newIcons/Precio Unitario.svg', type: 'Operaciones' },
-    { name: 'Presupuestos', icon: '../../../../../assets/newIcons/Presupuestos.svg', type: 'Operaciones' },
-    { name: 'Proceso de Seleccion', icon: '../../../../../assets/newIcons/Proceso de selección.svg', type: 'Recursos Humanos' },
-    { name: 'Recomendaciones de Empleados', icon: '../../../../../assets/newIcons/Recomendaciones de empleados.svg', type: 'Recursos Humanos' },
-    { name: 'Remesas', icon: '../../../../../assets/newIcons/Remesas.svg', type: 'Tesoreria' },
-    { name: 'Suscripciones', icon: '../../../../../assets/newIcons/Suscripciones.svg', type: 'Ventas' },
-    { name: 'Administracion de Habilidades', icon: '../../../../../assets/newIcons/Administración de habilidades.svg', type: 'Recursos Humanos' },
-    { name: 'Mesa de Ayuda', icon: '../../../../../assets/newIcons/../../../../../assets/newIcons/Mesa de Ayuda.svg', type: 'Productividad' },
-    { name: 'Gastos de Representacion', icon: '../../../../../assets/newIcons/Gastos de representación.svg', type: 'Recursos Humanos' },
-    { name: 'Bancos', icon: '../../../../../assets/newIcons/Bancos.svg', type: 'Tesoreria' },
-    { name: 'Politicas', icon: '../../../../../assets/newIcons/Políticas.svg', type: 'Productividad' },
-    { name: 'Evaluaciones', icon: '../../../../../assets/newIcons/Evaluaciones.svg', type: 'Recursos Humanos' },
-    { name: 'Contratos Empleados', icon: '../../../../../assets/newIcons/Contratos empleados.svg', type: 'Recursos Humanos' },
-    { name: 'Proveedores', icon: '../../../../../assets/newIcons/Proveedores.svg', type: 'Contabilidad' },
-    { name: 'Cuentas Bancarias', icon: '../../../../../assets/newIcons/Cuentas bancarias.svg', type: 'Tesoreria' },
-    { name: 'Validaciones', icon: '../../../../../assets/newIcons/Validaciones.svg', type: 'Productividad' },
-    { name: 'Pagos de Clientes', icon: '../../../../../assets/newIcons/Cuentas bancarias.svg', type: 'Tesoreria' },
-    { name: 'Pagos a proveedores', icon: '../../../../../assets/newIcons/Validaciones.svg', type: 'Tesoreria' },
+    { name: 'Aprobaciones', icon: '../../../../../assets/newIcons/Aprobaciones.svg', type: 'Recursos Humanos', url: '/approvals' },
+    { name: 'Arrendamiento', icon: '../../../../../assets/newIcons/Arrendamiento.svg', type: 'Ventas', url: '' },
+    { name: 'Asistencia', icon: '.../../../../../assets/newIcons/Asistencia.svg', type: 'Recursos Humanos', url: '' },
+    { name: 'Clientes', icon: '../../../../../assets/newIcons/Clientes.svg', type: 'Contabilidad', url: '/clients' },
+    { name: 'Comidas', icon: '../../../../../assets/newIcons/Comidas.svg', type: 'Recursos Humanos', url: '' },
+    { name: 'Compras', icon: './../../../../assets/newIcons/Compras.svg', type: 'Contabilidad', url: '' },
+    { name: 'Contratos y Subcontratos', icon: '../../../../../assets/newIcons/Contratos y subcontratos.svg', type: 'Operaciones', url: '/contracts' },
+    { name: 'CRM', icon: '../../../../../assets/newIcons/CRM.svg', type: 'Ventas', url: '' },
+    { name: 'Cuadro Comparativo', icon: '../../../../../assets/newIcons/Cuadro comparativo.svg', type: 'Operaciones', url: '/validation' },
+    { name: 'e-Learning', icon: '../../../../../assets/newIcons/eLearning.svg', type: 'Productividad', url: '' },
+    { name: 'Empleados', icon: '../../../../../assets/newIcons/Empleados.svg', type: 'Recursos Humanos', url: '' },
+    { name: 'Encuestas', icon: '../../../../../assets/newIcons/Encuestas.svg', type: 'Productividad', url: '' },
+    { name: 'Eventos', icon: '../../../../../assets/newIcons/Eventos.svg', type: 'Productividad', url: '' },
+    { name: 'Facturacion', icon: '../../../../../assets/newIcons/Facturación.svg', type: 'Contabilidad', url: '' },
+    { name: 'Flota', icon: '../../../../../assets/newIcons/Flota.svg', type: 'Operaciones', url: '' },
+    { name: 'Gestion de Activos', icon: '../../../../../assets/newIcons/Gestión de Archivos.svg', type: 'Contabilidad', url: '' },
+    { name: 'Inventario', icon: '../../../../../assets/newIcons/Inventario.svg', type: 'Contabilidad', url: '' },
+    { name: 'Mantenimiento', icon: '../../../../../assets/newIcons/Mantenimiento.svg', type: 'Operaciones', url: '' },
+    { name: 'Nomina', icon: '../../../../../assets/newIcons/Nómina.svg', type: 'Recursos Humanos', url: '' },
+    { name: 'Organigrama', icon: '../../../../../assets/newIcons/Organigrama.svg', type: 'Recursos Humanos', url: '' },
+    { name: 'Pedidos', icon: '../../../../../assets/newIcons/Pedidos.svg', type: 'Ventas', url: '' },
+    { name: 'Precio Unitario', icon: '../../../../../assets/newIcons/Precio Unitario.svg', type: 'Operaciones', url: '' },
+    { name: 'Presupuestos', icon: '../../../../../assets/newIcons/Presupuestos.svg', type: 'Operaciones', url: '' },
+    { name: 'Proceso de Seleccion', icon: '../../../../../assets/newIcons/Proceso de selección.svg', type: 'Recursos Humanos', url: '' },
+    { name: 'Recomendaciones de Empleados', icon: '../../../../../assets/newIcons/Recomendaciones de empleados.svg', type: 'Recursos Humanos', url: '' },
+    { name: 'Remesas', icon: '../../../../../assets/newIcons/Remesas.svg', type: 'Tesoreria', url: '' },
+    { name: 'Suscripciones', icon: '../../../../../assets/newIcons/Suscripciones.svg', type: 'Ventas', url: '' },
+    { name: 'Administracion de Habilidades', icon: '../../../../../assets/newIcons/Administración de habilidades.svg', type: 'Recursos Humanos', url: '' },
+    { name: 'Mesa de Ayuda', icon: '../../../../../assets/newIcons/../../../../../assets/newIcons/Mesa de Ayuda.svg', type: 'Productividad', url: '' },
+    { name: 'Gastos de Representacion', icon: '../../../../../assets/newIcons/Gastos de representación.svg', type: 'Recursos Humanos', url: '' },
+    { name: 'Bancos', icon: '../../../../../assets/newIcons/Bancos.svg', type: 'Tesoreria', url: '' },
+    { name: 'Politicas', icon: '../../../../../assets/newIcons/Políticas.svg', type: 'Productividad', url: '' },
+    { name: 'Evaluaciones', icon: '../../../../../assets/newIcons/Evaluaciones.svg', type: 'Recursos Humanos', url: '' },
+    { name: 'Contratos Empleados', icon: '../../../../../assets/newIcons/Contratos empleados.svg', type: 'Recursos Humanos', url: '' },
+    { name: 'Proveedores', icon: '../../../../../assets/newIcons/Proveedores.svg', type: 'Contabilidad', url: '/providers' },
+    { name: 'Cuentas Bancarias', icon: '../../../../../assets/newIcons/Cuentas bancarias.svg', type: 'Tesoreria', url: '' },
+    { name: 'Validaciones', icon: '../../../../../assets/newIcons/Validaciones.svg', type: 'Productividad', url: '' },
+    { name: 'Pagos de Clientes', icon: '../../../../../assets/newIcons/Cuentas bancarias.svg', type: 'Tesoreria', url: '/invoice-clients' },
+    { name: 'Pagos a proveedores', icon: '../../../../../assets/newIcons/Validaciones.svg', type: 'Tesoreria', url: '/invoice-providers' },
   ]
 
   public filters: any[] = [
-    { name: 'Todos'},
-    { name: 'Ventas'},
-    { name: 'Contabilidad'},
-    { name: 'Tesoreria'},
-    { name: 'Operaciones'},
-    { name: 'Productividad'},
-    { name: 'Recursos Humanos'}
+    { name: 'Todos' },
+    { name: 'Ventas' },
+    { name: 'Contabilidad' },
+    { name: 'Tesoreria' },
+    { name: 'Operaciones' },
+    { name: 'Productividad' },
+    { name: 'Recursos Humanos' }
   ]
 
   public selectedFilter: string = 'Todos'
 
-  constructor() {
+  constructor(
+    private _router: Router,
+  ) {
     this.servicesTemp = this.services
     this.servicesFilters = this.servicesTemp
   }
@@ -72,33 +75,35 @@ export class HomeComponent implements OnInit {
 
   }
 
-  changeAll(){
+  changeAll() {
     this.selectedFilter = 'Todos'
     this.services = this.servicesTemp
     this.servicesFilters = this.services
   }
 
-  selected(value:string){
-    if(value === 'Todos'){
+  selected(value: string) {
+    if (value === 'Todos') {
       this.services = this.servicesTemp
       this.servicesFilters = this.services
-    }else {
-      this.services = this.servicesTemp.filter((element:any) => element.type === value)
+    } else {
+      this.services = this.servicesTemp.filter((element: any) => element.type === value)
       this.servicesFilters = this.services
     }
   }
 
   search(term: string) {
-    if(term){
+    if (term) {
       const termino = term.toLowerCase().trim()
-      let data = this.servicesFilters.filter((item:any) => item.name.toLowerCase().trim().includes(termino))
+      let data = this.servicesFilters.filter((item: any) => item.name.toLowerCase().trim().includes(termino))
       this.services = data
-    }else {
-      if(this.selectedFilter === 'Todos'){
+      console.log('this.services', this.services);
+    } else {
+      if (this.selectedFilter === 'Todos') {
         this.services = this.servicesTemp
-      }else {
+      } else {
         this.services = this.servicesFilters
       }
     }
   }
+
 }
