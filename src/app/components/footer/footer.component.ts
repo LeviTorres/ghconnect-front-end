3b8d802a5dd3089b7d78391496218b36a2fc5094
Ezequiel Lino <sonisombra@gmail.com>
2023-02-17 14:28:50 -0600

    changes

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginService } from '../../services/login.service';
+import { ModalQuestionComponent } from '../modal-question/modal-question.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-footer',
@@ -7,9 +10,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+  public userTenant: any
+
+  constructor(
+    private _loginService: LoginService,
+    private _dialog: MatDialog
+  ) {
+    this.userTenant = _loginService.business
+  }
 
   ngOnInit(): void {
+  }
+
+  changeTenant() {
+    this._dialog.open(ModalQuestionComponent, {
+      width: '640px',
+      maxHeight: '95vh',
+      disableClose: true,
+      autoFocus: false,
+      data: this._loginService.business._id
+    });
   }
 
 }
