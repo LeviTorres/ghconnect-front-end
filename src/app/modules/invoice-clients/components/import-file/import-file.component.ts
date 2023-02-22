2bf8c9bd17d99bc9dcdb823c52d7ffd56d558da4
@LeviTorres1 <levi962009@hotmail.com>
2023-02-21 23:11:57 -0600

    changes

--- src/app/modules/invoice-clients/components/import-file/import-file.component.ts
+++ src/app/modules/invoice-clients/components/import-file/import-file.component.ts
@@ -9,6 +9,13 @@ import { MatDialogRef } from '@angular/material/dialog';
 import { InvoiceClientsService } from '../../../../services/invoice-clients.service';
 import * as XLSX from 'xlsx';
 import { PaymentConditions } from '../../../../models/PaymentConditions.model';
+import { MovementsTypeClientService } from '../../../../services/movements-type-client.service';
+import { DivisasService } from '../../../../services/divisas.service';
+import { Client } from '../../../../models/Client.model';
+import { Ceco } from '../../../../models/Ceco.model';
+import { Divisa } from '../../../../models/Divisa.model';
+import { MovementTypeClient } from '../../../../models/MovementTypeClient.model';
+import { LoginService } from '../../../../services/login.service';
 
 @Component({
   selector: 'app-import-file',
@@ -19,8 +26,14 @@ export class ImportFileComponent implements OnInit {
 
   public tenant: any
   public invoiceClients: InvoiceClient[] = []
-  public paymentConditions: any[] = []
+  public clients: Client[] = []
+  public cecos:Ceco[] = []
+  public divisas: Divisa[] = []
+  public movementTypes: MovementTypeClient[] = []
   public validateExcel: boolean = true
+
+  public history: any[] = []
+  public user:any
 
   public third_types_array: any[] = [
     { name: 'Proveedor' },
@@ -42,28 +55,53 @@ export class ImportFileComponent implements OnInit {
   constructor(
     private _cecoService: CecosService,
     private _clientService: ClientsService,
-    private _invoiceInvoiceService: InvoiceClientsService,
+    private _invoiceClientService: InvoiceClientsService,
     private _paymentConditionsService: PaymentConditionsService,
     private _spinner:NgxSpinnerService,
     private _toastr: ToastrService,
     private _dialogRef: MatDialogRef<ImportFileComponent>,
+    private _movementTypeService: MovementsTypeClientService,
+    private _divisaService: DivisasService,
+    private _loginService: LoginService
   ) {
     this.tenant = localStorage.getItem('tenant')
+    this.user = _loginService.user
   }
 
   ngOnInit(): void {
-    this.getPaymentConditions()
     this.getInvoiceClients()
+    this.getClients()
+    this.getCecos()
+    this.getMovementTypes()
+    this.getDivisas()
   }
 
-  getPaymentConditions(){
-    this._paymentConditionsService.getPaymentConditions().subscribe((payments:any) => {
-      this.paymentConditions = payments
+  getDivisas(){
+    this._divisaService.getDivisas().subscribe((divisas:Divisa[]) => {
+      this.divisas = divisas
+    })
+  }
+
+  getMovementTypes(){
+    this._movementTypeService.getMovementsTypeClient().subscribe((movementTypes:MovementTypeClient[]) => {
+      this.movementTypes = movementTypes
+    })
+  }
+
+  getCecos(){
+    this._cecoService.getCecos().subscribe((cecos: Ceco[]) => {
+      this.cecos = cecos
+    })
+  }
+
+  getClients(){
+    this._clientService.getClients().subscribe((clients:Client[]) => {
+      this.clients = clients
     })
   }
 
   getInvoiceClients(){
-    this._invoiceInvoiceService.getInvoiceClients().subscribe((invoiceClients:any) => {
+    this._invoiceClientService.getInvoiceClients().subscribe((invoiceClients:any) => {
       this.invoiceClients = invoiceClients
     })
   }
@@ -79,61 +117,64 @@ export class ImportFileComponent implements OnInit {
       workbook.SheetNames.forEach(sheet => {
         const data = XLSX.utils.sheet_to_json(workbook.Sheets[sheet])
         data.forEach((element:any) => {
-          const findPayment:any = this.paymentConditions.find((e:PaymentConditions) => e.name_payment.toLowerCase().trim() === element.Condiciones_de_pago.toLowerCase().trim())
-          if(!findPayment){
-            this._toastr.error('Condiciones de pago incorrecto')
+          const findMovementType:any = this.movementTypes.find((e:MovementTypeClient) => e.name_movement.toLowerCase().trim() === element.Tipo_de_movimiento.toLowerCase().trim())
+          if(!findMovementType){
+            this._toastr.error('Tipo de movimiento incorrecto')
             this._spinner.hide()
             this.validateExcel = false
             return
           }
-          const findThirdType:any = this.third_types_array.find((e:any) => e.name.toLowerCase().trim() === element.Tipo_de_tercero.toLowerCase().trim())
-          if(!findThirdType){
-            this._toastr.error('Tipo de tercero incorrecto')
+          const findClient:any = this.clients.find((e:Client) => e.name.toLowerCase().trim() === element.Cliente.toLowerCase().trim())
+          if(!findClient){
+            this._toastr.error('Cliente incorrecto')
             this._spinner.hide()
             this.validateExcel = false
             return
           }
-          const findSocietyType:any = this.society_types_array.find((e:any) => e.name.toLowerCase().trim() === element.Tipo_de_sociedad.toLowerCase().trim())
-          if(!findSocietyType){
-            this._toastr.error('Tipo de sociedad incorrecto')
+          const findCeco:any = this.cecos.find((e: Ceco) => e.name_short.toLowerCase().trim() === element.Ceco_corto.toLowerCase().trim())
+          if(!findCeco){
+            this._toastr.error('Ceco incorrecto')
             this._spinner.hide()
             this.validateExcel = false
             return
           }
-          const findProviderType:any = this.provider_array.find((e:any) => e.name.toLowerCase().trim() === element.Tipo_de_proveedor.toLowerCase().trim())
-          if(!findProviderType){
-            this._toastr.error('Tipo de proveedor incorrecto')
+          const findDivisa:any = this.divisas.find((e: Divisa) => e.abbreviation_divisa.toLowerCase().trim() === element.Divisa.toLowerCase().trim())
+          if(!findDivisa){
+            this._toastr.error('Divisa incorrecto')
             this._spinner.hide()
             this.validateExcel = false
             return
           }
-        /*  const findKeyProvider:any = this.providers.find((e:any) => e.key_provider === element.No_proveedor)
-          if(findKeyProvider){
-            this._toastr.error('Numero de proveedor repetido')
-            this._spinner.hide()
-            this.validateExcel = false
-            return
-          }*/
         })
 
         if(this.validateExcel){
+          this.history.push({
+            user: this.user._id,
+            note: `Factura creada`,
+            date: new Date().getTime(),
+            type: 'note'
+          })
            data.forEach((element:any) => {
-            const findPayment:any = this.paymentConditions.find((e:PaymentConditions) => e.name_payment.toLowerCase().trim() === element.Condiciones_de_pago.toLowerCase().trim())
+            const findMovementType:any = this.movementTypes.find((e:MovementTypeClient) => e.name_movement.toLowerCase().trim() === element.Tipo_de_movimiento.toLowerCase().trim())
+            const findClient:any = this.clients.find((e:Client) => e.name.toLowerCase().trim() === element.Cliente.toLowerCase().trim())
+            const findCeco:any = this.cecos.find((e: Ceco) => e.name_short.toLowerCase().trim() === element.Ceco_corto.toLowerCase().trim())
+            const findDivisa:any = this.divisas.find((e: Divisa) => e.abbreviation_divisa.toLowerCase().trim() === element.Divisa.toLowerCase().trim())
             const datos = {
+              activities: this.history,
               tenant: this.tenant,
-              key_client: element.No_cliente,
-              name: element.Nombre_cliente,
-              nit: element.NIT,
-              third_type: element.Tipo_de_tercero,
-              society_type: element.Tipo_de_sociedad,
-              provider_type: element.Tipo_de_proveedor,
-              phone_number: element.Telefono_local,
-              mobile_number: element.Telefono_movil,
-              email: element.Email,
-              payment_conditions: findPayment?._id,
+              movement_type: findMovementType._id,
+              ceco: findCeco._id,
+              client: findClient._id,
+              key_invoice: element.No_factura,
+              upload_date: new Date(element.Fecha_carga).getTime(),
+              invoice_date: new Date(element.Fecha_factura).getTime(),
+              expiration_date: new Date(element.Fecha_vencimiento).getTime(),
+              invoice_total: element.Total_factura,
+              divisa: findDivisa._id,
+              description: element.Descripcion,
             }
             console.log('data', datos)
-            this._clientService.createClient(datos).subscribe((resp:any) => {})
+            this._invoiceClientService.createInvoiceClient(datos).subscribe((resp:any) => {})
         });
         }
         this._dialogRef.close()
